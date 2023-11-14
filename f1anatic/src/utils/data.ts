837810e084b0b6b0bb3f885f394656fe9453f229
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-14 13:21:42 +0200

    Create nav links data

---
+++ f1anatic/src/utils/data.ts
@@ -0,0 +1,7 @@
+export const navLinks = [ 
+    { name: '.blogs', path: '/' },
+    { name: '.drivers', path: '/' },
+    { name: '.teams', path: '/' },
+    { name: '.contact', path: '/' },
+    { name: '.login', path: '/' },
+]
