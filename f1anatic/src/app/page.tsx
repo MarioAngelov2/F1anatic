75fe598d34dc340fffc9a72094431754ec4ee8ab
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-14 21:19:33 +0200

    Update page.tsx

--- f1anatic/src/app/page.tsx
+++ f1anatic/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import Hero from "@/components/Hero/Hero";
+import HeroBlogs from "@/components/HeroBlogs/HeroBlogs";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <div>
             <Container>
                 <Hero />
+                <HeroBlogs />
             </Container>
         </div>
     );
