57d6a28c4048d65569d733f8cfd334f66134b562
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-14 14:04:27 +0200

    Add footer links data

--- f1anatic/src/utils/data.ts
+++ f1anatic/src/utils/data.ts
@@ -1,7 +1,22 @@
-export const navLinks = [ 
-    { name: '.blogs', path: '/' },
-    { name: '.drivers', path: '/' },
-    { name: '.teams', path: '/' },
-    { name: '.contact', path: '/' },
-    { name: '.login', path: '/' },
+export const navLinks = [
+    { name: ".blogs", path: "/" },
+    { name: ".drivers", path: "/" },
+    { name: ".teams", path: "/" },
+    { name: ".contact", path: "/" },
+    { name: ".login", path: "/" },
+];
+
+export const webLinks = [
+    { name: "All Blogs", path: "/" },
+    { name: "Current Drivers", path: "/" },
+    { name: "Current Teams", path: "/" },
+    { name: "Profile", path: "/" },
+    { name: "Login", path: "/" },
+    { name: "Sign Up", path: "/" },
+];
+
+export const webLinks2 = [
+    { name: "About us", path: "/" },
+    { name: "Contact us", path: "/" },
+    { name: "SEO map", path: "/" },
 ]
