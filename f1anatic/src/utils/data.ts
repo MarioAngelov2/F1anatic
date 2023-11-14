cbe2f96b32a3573984eeae781d9e9defb6912614
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-14 22:25:46 +0200

    Add blog list data

--- f1anatic/src/utils/data.ts
+++ f1anatic/src/utils/data.ts
@@ -1,3 +1,7 @@
+import image1 from "../../public/main (1).png";
+import image2 from "../../public/main (2).png";
+import image3 from "../../public/main (3).png";
+
 export const navLinks = [
     { name: ".blogs", path: "/" },
     { name: ".drivers", path: "/" },
@@ -19,4 +23,25 @@ export const webLinks2 = [
     { name: "About us", path: "/" },
     { name: "Contact us", path: "/" },
     { name: "SEO map", path: "/" },
-]
+];
+
+export const blogPosts = [
+    {
+        title: "US Grand Prix Race Review",
+        summary:
+            "The Circuit of The Americas provided us with some heated battles... Let's take a closer look at how the race unfolded",
+        image: image1,
+    },
+    {
+        title: "Japanese Grand Prix Race Review",
+        summary:
+            "Can Max be crowned 2-time World Champion despite the hectic conditions?",
+        image: image2,
+    },
+    {
+        title: "Singapore Grand Prix Race Review",
+        summary:
+            "How the drivers handled the physically most challenging track in wet conditions",
+        image: image3,
+    },
+];
