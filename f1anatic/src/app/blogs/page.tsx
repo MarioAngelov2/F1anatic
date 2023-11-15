11927ad1c8b16ad2f2eca71ff2ac61f67ec203d8
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-15 18:50:24 +0200

    Create blog component

---
+++ f1anatic/src/app/blogs/page.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import Container from "@/components/Container";
+import { BlogsRecord, getXataClient } from "@/xata";
+import BlogList from "./BlogList";
+
+const Blogs = async () => {
+    const xataClient = getXataClient();
+    const blogsData = await xataClient.db.blogs.getMany();
+
+    return (
+        <div className="px-8 mt-7 h-screen">
+            <Container>
+                {/* <BlogList blogs={blogs}/> */}
+                <BlogList blogs={blogsData} />
+            </Container>
+        </div>
+    );
+};
+
+export default Blogs;
