88af70d747615132911d5367e5ef945a424ea900
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-16 20:56:40 +0200

    Update page.tsx

--- f1anatic/src/app/blogs/[blogId]/page.tsx
+++ f1anatic/src/app/blogs/[blogId]/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/Container";
 import { BlogsRecord, getXataClient } from "@/xata";
 import moment from "moment";
 import Image from "next/image";
+import { VscSend } from "react-icons/vsc";
 
 const xataClient = getXataClient();
 
@@ -12,7 +13,7 @@ const BlogDetails = async ({ params }: { params: { blogId: string } }) => {
     return (
         <div className="relative">
             {blogData?.blogImages?.map((image) => (
-                <div className="relative">
+                <div key={image.id} className="relative">
                     <div className="h-[500px] flex items-center justify-center relative overflow-hidden">
                         <Image
                             src={image.url}
@@ -40,29 +41,53 @@ const BlogDetails = async ({ params }: { params: { blogId: string } }) => {
             <div className="w-screen bg-white pt-1 pb-8">
                 <Container>
                     <div className="h-full px-4 md:px-8 lg:px-24 z-20 bg-white w-full">
-                        <h1 className="mt-8 mb-2 font-bold text-lg">
-                            {blogData?.articleOneTitle}
-                        </h1>
-                        <p>{blogData?.articleOne}</p>
-                        {blogData?.articleImage?.map((image) => (
-                            <div className="mt-6 max-h-[600px]">
-                                <Image
-                                    src={image.url}
-                                    alt={image.name}
-                                    width={1200}
-                                    height={300}
-                                    className="object-cover md:h-[600px]"
+                        <div>
+                            <h1 className="mt-8 mb-2 font-bold text-lg">
+                                {blogData?.articleOneTitle}
+                            </h1>
+                            <p>{blogData?.articleOne}</p>
+                            {blogData?.articleImage?.map((image) => (
+                                <div key={image.id} className="flex justify-center mt-6 max-h-[500px]">
+                                    <Image
+                                        src={image.url}
+                                        alt={image.name}
+                                        width={1200}
+                                        height={300}
+                                        className="object-cover md:h-[500px] w-[70%]"
+                                    />
+                                </div>
+                            ))}
+                            <h2 className="mt-8 mb-2 font-bold text-lg">
+                                {blogData?.articleTwoTitle}
+                            </h2>
+                            <p>{blogData?.articleTwo}</p>
+                            <h3 className="mt-8 mb-2 font-bold text-lg">
+                                {blogData?.ArticleThreeTitle}
+                            </h3>
+                            <p>{blogData?.ArticleThree}</p>
+                            <p className="text-gray-500 italic text-sm md:text-base mt-4">
+                                Източник: Club S1
+                            </p>
+                        </div>
+                        {/* ADD COMMENT SECTION */}
+                        <div className="flex flex-col justify-between bg-gray-200 w-full h-32 mt-16 p-3 md:p-6 mb-12">
+                            <h1 className="text-xl md:text-2xl font-bold text-gray-800">
+                                Сподели своя коментар!
+                            </h1>
+                            <div className="flex flex-row items-center justify-between">
+                                <input
+                                    type="text"
+                                    placeholder="Добави коментар..."
+                                    className="w-[90%] border-b-2 border-gray-300 
+                                    bg-transparent outline-none px-2 py-1"
+                                />
+                                <VscSend
+                                    size={38}
+                                    className="text-gray-400 border-b-2 border-gray-300 py-1
+                                    w-20 cursor-pointer hover:text-gray-800 transition duration-300 ease-in-out"
                                 />
                             </div>
-                        ))}
-                        <h2 className="mt-8 mb-2 font-bold text-lg">
-                            {blogData?.articleTwoTitle}
-                        </h2>
-                        <p>{blogData?.articleTwo}</p>
-                        <h3 className="mt-8 mb-2 font-bold text-lg">
-                            {blogData?.ArticleThreeTitle}
-                        </h3>
-                        <p>{blogData?.ArticleThree}</p>
+                        </div>
                     </div>
                 </Container>
             </div>
