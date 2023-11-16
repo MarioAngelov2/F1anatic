bc61c5845a4748ed941826275b8ba47f052607f5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-16 15:08:26 +0200

    Minor responsive refactor & add more data

--- f1anatic/src/app/blogs/[blogId]/page.tsx
+++ f1anatic/src/app/blogs/[blogId]/page.tsx
@@ -13,18 +13,21 @@ const BlogDetails = async ({ params }: { params: { blogId: string } }) => {
         <div className="relative">
             {blogData?.blogImages?.map((image) => (
                 <div className="relative">
-                    <div className="h-[500px] flex items-center justify-center relative">
+                    <div className="h-[500px] flex items-center justify-center relative overflow-hidden">
                         <Image
                             src={image.url}
                             alt={image.name}
                             width={1200}
                             height={500}
                             className="object-cover w-screen min-w-[1000px] max-h-[500px] -z-10 
-                            items-center justify-center"
+                            items-center justify-center fixed"
                         />
-                        <div className="absolute inset-0 bg-black opacity-50 w-full h-full"></div>
+                        <div className="absolute inset-auto bg-black opacity-50 w-full h-full"></div>
                     </div>
-                    <div className="absolute top-52 right-0 left-0 text-white text-center pl-10 pr-10">
+                    <div
+                        className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
+                        text-white lg:text-center pl-10 pr-10 w-full md:w-[80%] xl:w-[70%] 2xl:w-[50%]"
+                    >
                         <h1 className="text-4xl font-bold">
                             {blogData.blogTitle}
                         </h1>
@@ -34,12 +37,35 @@ const BlogDetails = async ({ params }: { params: { blogId: string } }) => {
                     </div>
                 </div>
             ))}
-            <Container>
-                <div className="h-screen px-8 lg:px-24">
-                    <h1 className="mt-8 mb-2 font-bold text-lg">{blogData?.blogSecondTitle}</h1>
-                    <p>{blogData?.blogArticle}</p>
-                </div>
-            </Container>
+            <div className="w-screen bg-white pt-1">
+                <Container>
+                    <div className="min-h-screen px-4 md:px-8 lg:px-24 z-20 bg-white w-full">
+                        <h1 className="mt-8 mb-2 font-bold text-lg">
+                            {blogData?.articleOneTitle}
+                        </h1>
+                        <p>{blogData?.articleOne}</p>
+                        {blogData?.articleImage?.map((image) => (
+                            <div className="mt-6 max-h-[600px]">
+                                <Image
+                                    src={image.url}
+                                    alt={image.name}
+                                    width={1200}
+                                    height={300}
+                                    className="object-cover md:h-[600px]"
+                                />
+                            </div>
+                        ))}
+                        <h2 className="mt-8 mb-2 font-bold text-lg">
+                            {blogData?.articleTwoTitle}
+                        </h2>
+                        <p>{blogData?.articleTwo}</p>
+                        <h3 className="mt-8 mb-2 font-bold text-lg">
+                            {blogData?.ArticleThreeTitle}
+                        </h3>
+                        <p>{blogData?.ArticleThree}</p>
+                    </div>
+                </Container>
+            </div>
         </div>
     );
 };
