06ee7b41fecaa4e028cdbc43d2a2d7330d757c52
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-16 15:08:11 +0200

    Minor responsive refactor

--- f1anatic/src/app/blogs/BlogList.tsx
+++ f1anatic/src/app/blogs/BlogList.tsx
@@ -11,16 +11,19 @@ type BlogListProps = {
 
 const BlogList: React.FC<BlogListProps> = ({ blogs }) => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto gap-4">
+        <div className="grid grid-cols-1 xl:grid-cols-2 mx-auto gap-4">
             {blogs.map((blog) => (
                 <Link href={`/blogs/${blog.id}`}>
                     <div
                         key={blog.id}
                         className="flex flex-row gap-4 bg-gray-100 hover:shadow-lg hover:bg-white
-                    transition duration-300 ease-in-out cursor-pointer"
+                        transition duration-300 ease-in-out cursor-pointer"
                     >
                         {blog.blogImages?.map((image) => (
-                            <div className="flex items-center justify-center w-[40%] max-h-[220px] overflow-hidden">
+                            <div
+                                key={image.id}
+                                className="flex items-center justify-center w-[40%] max-h-[220px] overflow-hidden"
+                            >
                                 <Image
                                     src={image.url}
                                     alt={image.name}
@@ -31,15 +34,18 @@ const BlogList: React.FC<BlogListProps> = ({ blogs }) => {
                             </div>
                         ))}
                         <div className="flex flex-col px-2 py-2 md:py-4 max-w-[60%] max-h-[300px] justify-between">
-                            <h1 className="font-bold text-lg md:text-xl">
-                                {blog.blogTitle}
+                            <h1 className="font-bold text-lg md:text-xl leading-6">
+                                {truncateText(blog.blogTitle!, 50)}
                             </h1>
                             <p className="text-xs md:text-base md:mt-2">
-                                {truncateText(blog.blogSecondTitle!, 100)}
+                                {truncateText(blog.titePreview!, 90)}
                             </p>
                             <div>
                                 <p className="text-xs md:text-sm text-gray-400 mt-2 lg:mt-0 italic">
-                                    Posted {moment(blog.createdAt).fromNow()}
+                                    Публикувано{" "}
+                                    {moment(blog.createdAt).format(
+                                        "DD MMM YYYY"
+                                    )}
                                 </p>
                             </div>
                         </div>
