2b2f42e77cbf82e2ffa4793be9a173c517d5b0a9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-16 21:50:19 +0200

    Fetch & display latest 3 blogs

--- f1anatic/src/components/HeroBlogs/HeroBlogs.tsx
+++ f1anatic/src/components/HeroBlogs/HeroBlogs.tsx
@@ -3,36 +3,49 @@ import Container from "../Container";
 import Image from "next/image";
 import { blogPosts } from "@/utils/data";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { BlogsRecord, getXataClient } from "@/xata";
 import Link from "next/link";
+import { truncateText } from "@/utils/truncateText";
 
-const HeroBlogs = () => {
+const xataClient = getXataClient();
+
+const HeroBlogs = async () => {
+    const blogData = await xataClient.db.blogs.getPaginated({
+        pagination: { size: 3 },
+    });
+
     return (
         <Container>
             <div className="px-8 lg:px-24 mt-24 mb-12">
                 <div className="flex flex-col gap-12">
-                    {blogPosts.map((blog) => (
+                    {blogData.records.map((blog) => (
                         <div
-                            key={blog.title}
+                            key={blog.blogTitle}
                             className="flex flex-col lg:flex-row gap-6 lg:items-start
                             bg-gray-100 lg:bg-transparent"
                         >
                             <div className="flex items-center justify-center lg:max-w-[400px] min-w-[200px] lg:min-w-[400px]">
                                 <Image
-                                    src={blog.image}
+                                    src={
+                                        blog.blogImages
+                                            ? blog.blogImages[0].url
+                                            : "defaultImage"
+                                    }
                                     alt="blogImage"
                                     width={1200}
                                     height={300}
+                                    className="object-cover h-[300px]"
                                 />
                             </div>
-                            <div className="flex flex-col px-5 min-w-[200px]">
-                                <h1 className="lg:mt-7 mb-2 text-xl font-bold">
-                                    {blog.title}
+                            <div className="flex flex-col justify-evenly h-[400px] md:h-[300px] px-5 min-w-[200px]">
+                                <h1 className="text-xl font-bold">
+                                    {truncateText(blog.blogTitle!, 120)}
                                 </h1>
-                                <p className="">{blog.summary}</p>
-                                <button className="btnPrimary group mt-8 mb-6 lg:mb-0">
-                                    Read more
+                                <p className="">{truncateText(blog.titePreview!, 240)}</p>
+                                <Link href={`/blogs/${blog.id}`} className="btnPrimary group w-[200px]">
+                                    Прочети повече
                                     <FaArrowRightLong className="group-hover:translate-x-1 transition ease-in-out" />
-                                </button>
+                                </Link>
                             </div>
                         </div>
                     ))}
@@ -44,7 +57,7 @@ const HeroBlogs = () => {
                         font-semibold text-gray-400 hover:text-black transition duration-300 ease-in-out
                         group mt-8 mb-6 lg:mb-0 text-base lg:text-lg"
                     >
-                        View More Blogs
+                        Виж повече статии
                     </Link>
                 </div>
             </div>
